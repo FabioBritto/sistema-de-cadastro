f47f6d5dcfaa9ccb278a08d4faee99aad9ca28f9
fabio.tritono@gmail.com <fabio.tritono@gmail.com>
2023-06-07 16:20:39 -0300

    adicionando o contato do Fabio

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Fabio Britto é muito, muito lindo")
-console.log("Essa aqui é da Branch 2")
+console.log("Essa aqui é da Branch 2 com muito amor")
